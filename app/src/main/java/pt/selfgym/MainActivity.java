b7ec4f23b6cf4ab02c7848e4e322076eea0e7928
lamyaekerouad25 <kerouad.lamyae@inemail.ine.inpt.ma>
2024-01-13 21:19:05 +0100

    add login and register

--- app/src/main/java/pt/selfgym/MainActivity.java
+++ app/src/main/java/pt/selfgym/MainActivity.java
@@ -1,18 +1,18 @@
 package pt.selfgym;
 
-import static androidx.core.app.NotificationCompat.PRIORITY_HIGH;
+import static android.app.job.JobInfo.PRIORITY_HIGH;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,17 +28,17 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import pt.selfgym.databinding.ActivityMainBinding;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.gson.Gson;
-
 
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.util.Date;
 
 import pt.selfgym.Interfaces.ActivityInterface;
+import pt.selfgym.Interfaces.LoginActivity;
+import pt.selfgym.SharedViewModel;
+import pt.selfgym.databinding.ActivityMainBinding;
 import pt.selfgym.dtos.WorkoutDTO;
 import pt.selfgym.services.NotificationService;
 import pt.selfgym.ui.calendar.CalendarFragment;
@@ -46,7 +46,6 @@ import pt.selfgym.ui.calendar.RunWorkoutFragment;
 import pt.selfgym.ui.workouts.AddExerciseFragment;
 import pt.selfgym.ui.workouts.EditWorkoutFragment;
 import pt.selfgym.ui.workouts.WorkoutFragment;
-import pt.selfgym.utils.NotificationUtil;
 
 public class MainActivity extends AppCompatActivity implements ActivityInterface {
 
@@ -58,16 +57,26 @@ public class MainActivity extends AppCompatActivity implements ActivityInterface
     private NotificationManager notificationManager;
     private NotificationCompat.Builder notificationBuilder;
 
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private static final String LAST_VISITED_FRAGMENT_KEY = "last_visited_fragment";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         model = new ViewModelProvider(this).get(SharedViewModel.class);
         model.startDB();
-        //model.deleteAllWorkouts();
         model.getToastMessageObserver().observe(this, message -> {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         });
+
+        // Check if the user is logged in
+        if (!isLoggedIn()) {
+            // If not logged in, navigate to LoginActivity
+            navigateToLogin();
+            return;
+        }
+
         mService = new NotificationService();
         mServiceIntent = new Intent(this, mService.getClass());
         if (!NotificationService.IS_SERVICE_RUNNING) {
@@ -77,22 +86,12 @@ public class MainActivity extends AppCompatActivity implements ActivityInterface
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-//        // assigning ID of the toolbar to a variable
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_calendar, R.id.navigation_workouts, R.id.navigation_statistics)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
-
             @Override
             public void onDestinationChanged(@NonNull NavController navController, @NonNull NavDestination destination, @Nullable Bundle arguments) {
                 if (destination.getId() == R.id.navigation_workouts || destination.getId() == R.id.navigation_calendar || destination.getId() == R.id.navigation_statistics) {
@@ -105,6 +104,19 @@ public class MainActivity extends AppCompatActivity implements ActivityInterface
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    private boolean isLoggedIn() {
+        // Add your logic to check if the user is logged in
+        // For simplicity, I'm assuming a SharedPreference value here
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        return sharedPreferences.getBoolean("isLoggedIn", false);
+    }
+
+    private void navigateToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();  // Finish the current activity so that the user cannot navigate back
     }
 
     @Override
@@ -139,11 +151,6 @@ public class MainActivity extends AppCompatActivity implements ActivityInterface
 
     @Override
     protected void onDestroy() {
-        //stopService(mServiceIntent);
-//        Intent broadcastIntent = new Intent();
-//        broadcastIntent.setAction("restartservice");
-//        broadcastIntent.setClass(this, Restarter.class);
-//        this.sendBroadcast(broadcastIntent);
         super.onDestroy();
     }
 
@@ -163,24 +170,23 @@ public class MainActivity extends AppCompatActivity implements ActivityInterface
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(),
                         R.mipmap.ic_clock_small_black_24dp_round))
                 .setPriority(PRIORITY_HIGH)
-//                .setSound()
                 .setAutoCancel(true)
                 .build();
         notificationManager.notify((int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE), notification);
     }
 
     @Override
-    public void msgmqttpopup(String topic, MqttMessage message){
+    public void msgmqttpopup(String topic, MqttMessage message) {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        final View mqttmesagePopUp = getLayoutInflater().inflate(R.layout.mqtt_message_popup, null);
-        Button confirm = (Button) mqttmesagePopUp.findViewById(R.id.confirmmsgbtnmqtt);
-        Button cancel = (Button) mqttmesagePopUp.findViewById(R.id.cancelmsgbtnmqtt2);
-        TextView topico = (TextView) mqttmesagePopUp.findViewById(R.id.topicmsgmqtt);
-        TextView titulo = (TextView) mqttmesagePopUp.findViewById(R.id.titlemsgmqtt);
+        final View mqttMessagePopUp = getLayoutInflater().inflate(R.layout.mqtt_message_popup, null);
+        Button confirm = mqttMessagePopUp.findViewById(R.id.confirmmsgbtnmqtt);
+        Button cancel = mqttMessagePopUp.findViewById(R.id.cancelmsgbtnmqtt2);
+        TextView topico = mqttMessagePopUp.findViewById(R.id.topicmsgmqtt);
+        TextView titulo = mqttMessagePopUp.findViewById(R.id.titlemsgmqtt);
         WorkoutDTO workoutDTO = new Gson().fromJson(message.toString(), WorkoutDTO.class);
         topico.setText(topic);
         titulo.setText(workoutDTO.getName());
-        dialogBuilder.setView(mqttmesagePopUp);
+        dialogBuilder.setView(mqttMessagePopUp);
         Dialog dialog = dialogBuilder.create();
         dialog.show();
 
@@ -199,7 +205,6 @@ public class MainActivity extends AppCompatActivity implements ActivityInterface
         });
     }
 
-
     @Override
     public void changeFrag(Fragment fr, Bundle bundle) {
         if (fr instanceof EditWorkoutFragment)
@@ -215,4 +220,15 @@ public class MainActivity extends AppCompatActivity implements ActivityInterface
         }
     }
 
+    private void storeLastVisitedFragment(int fragmentId) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(LAST_VISITED_FRAGMENT_KEY, fragmentId);
+        editor.apply();
+    }
+
+    private int getLastVisitedFragment() {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        return preferences.getInt(LAST_VISITED_FRAGMENT_KEY, R.id.navigation_calendar);
+    }
 }
